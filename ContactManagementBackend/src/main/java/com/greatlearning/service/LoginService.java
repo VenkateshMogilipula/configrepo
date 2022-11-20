478e4b41b1b457f602cf7e143cfe1e5e72ab9961
VenkateshMogilipula <117717342+VenkateshMogilipula@users.noreply.github.com>
2022-11-20 10:15:56 +0530

    Delete ContactManagementBackend directory

--- ContactManagementBackend/src/main/java/com/greatlearning/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.service;
-
-import com.greatlearning.entity.User;
-
-public interface LoginService {
-	
-	public User getLoginDetails(String userName,String password);
-
-}
--- ContactManagementBackend/src/main/java/com/greatlearning/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.service;
-
-import com.greatlearning.entity.User;
-
-public interface UserService {
-	
-	public void save(User user);
-
-}
